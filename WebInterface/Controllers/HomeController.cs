94507615bb3edcabacf2df82c5c8e1171b6baf5a
Joe <jal126463@gmail.com>
2016-12-09 18:59:08 -0500

    First Commit

---
+++ WebInterface/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using SpotifyAPI.Local;
+using SpotifyAPI.Local.Enums;
+using SpotifyAPI.Local.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebInterface.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Track _currentTrack;
+        // GET: Index
+        public ActionResult Index()
+        { 
+
+            if (!SpotifyLocalAPI.IsSpotifyRunning())
+            {
+                ViewBag.song = "Spotify is not running!";
+            }
+            else
+            {
+                StatusResponse status = MvcApplication._spotify.GetStatus();
+                _currentTrack = status.Track;
+                ViewBag.song = _currentTrack.TrackResource.Name;
+            }
+            return View();
+        }
+
+        public ActionResult Skip()
+        {
+            MvcApplication._spotify.Skip();
+            return View("Index");
+        }
+    }
+}
---
+++ WebInterface/Global.asax.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using SpotifyAPI.Local;
+using SpotifyAPI.Local.Enums;
+using SpotifyAPI.Local.Models;
+
+namespace WebInterface
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        public static SpotifyLocalAPI _spotify;
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            _spotify = new SpotifyLocalAPI();
+            if (SpotifyLocalAPI.IsSpotifyRunning())
+            {
+                _spotify.Connect();
+                _spotify.ListenForEvents = true;
+            }
+        }
+    }
+}
